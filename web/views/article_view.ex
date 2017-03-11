5365c06e52d66b342d15261620795b39dff823af
msp <matt@cenatus.org>
2017-03-11 23:35:22 +0000

    Tag styling  - logo spacing  - List alignment  - Remove animation on logo

--- web/views/article_view.ex
+++ web/views/article_view.ex
@@ -1,3 +1,14 @@
 defmodule CenatusLtd.ArticleView do
   use CenatusLtd.Web, :view
+
+  def render_tags_as_links_from(conn, tags) do
+    tag_as_link = fn (tag) ->
+      content_tag(:span,
+                  content_tag(:a, tag.name, href: tag_path(conn, :show, tag), class: ""),
+                  class: "tag")
+
+    end
+
+    Enum.map(tags, tag_as_link)
+  end
 end
