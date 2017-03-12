d22b584df9772909a1612120bdd61368f09187e2
msp <matt@cenatus.org>
2017-03-12 01:16:49 +0000

    First pass at page animation on home page

--- web/static/js/onload.js
+++ web/static/js/onload.js
@@ -19,4 +19,17 @@ $(document).ready(function() {
         $(this).css('display','flex');
     });
   });
+
+  var tl = new TimelineLite();
+
+  tl.to('.home .container', 1, { opacity:1 })
+    .from(".home .header ", 0.3, { scale:0.8, opacity:0, delay:0.1, ease:Expo.easeOut, force3D:true}, 0.1)
+    .from(".home .logo ", 0.3, { scale:0.8, opacity:0, delay:0.1, ease:Expo.easeOut, force3D:true}, 0.2)
+    .staggerFrom(".home .category ", 0.3, { scale:0.8, opacity:0, delay:0.1, ease:Expo.easeOut, force3D:true}, 0.1)
+    .from('.home .articles', 1, { opacity:0 })
+    .staggerFrom(".home .articles .preview", 0.3, { scale:0.8, opacity:0, delay:0.1, ease:Expo.easeOut, force3D:true}, 0.1)
+    .from(".home .meta ", 0.3, { scale:0.8, opacity:0, delay:0.1, ease:Expo.easeOut, force3D:true}, 0.1)
+    .staggerFrom(".home .meta aside", 0.3, { scale:0.8, opacity:0, delay:0.1, ease:Expo.easeOut, force3D:true}, 0.2)
+    .from('.home footer', 1, { opacity:0 })
+
 });
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule CenatusLtd.PageView do
   use CenatusLtd.Web, :view
+
+  def handler_info(conn) do
+    action_name conn
+  end
 end
