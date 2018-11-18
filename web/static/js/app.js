657d8a92ab436f794ae8383bfc575268df8aa6de
msp <matt@cenatus.org>
2018-11-18 18:57:47 +0000

    Tidy jQuery mess

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,8 +12,6 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-// import "jquery"
-// import "cloudinary-jquery"
 
 // Import local files
 //
